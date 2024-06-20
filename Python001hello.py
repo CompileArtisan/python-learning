92b56cb66d2676938c5bc4e83eb9d2a1c08499c3
CompileArtisan <praanesh.b.nair@gmail.com>
2024-06-20 15:34:25 +0530

    feat: Add alternative formatted string method in Python
    This commit adds an alternative method for creating formatted strings in Python using the `format()` function. It demonstrates how to use placeholders `{}` to substitute variables in a string. This method provides flexibility and readability when formatting strings.
    
    Also, remove unnecessary comments and blank lines.

--- Python001hello.py
+++ Python001hello.py
@@ -15,5 +15,8 @@ print("\n\n") # \n is newline character. \n\n is two newlines
 city = input("Where are you from? ")
 print(f"Hello {name}. You are from {city}.") # f-string, which stands for formatted string
 
+# here's another way of formatted strings:
+print("Hello {}. You are from {}.".format(name,city)) # {} are placeholders for variables
+
 # print() is most comparable to System.out.println() in java
 
