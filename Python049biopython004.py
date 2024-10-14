d7ce572694c642b29368e5f43bd912926fdd6b76
CompileArtisan <praanesh.b.nair@gmail.com>
2024-10-14 11:23:04 +0530

    Refactor code to generate and save a molecule image with descriptors

--- Python049biopython004.py
+++ Python049biopython004.py
@@ -2,4 +2,17 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import PDBWriter
 
-# 
+# Create Molecule from SMILES
+smiles = "CC(=O)NC1=CC=C(C=C1)O"
+molecule = Chem.MolFromSmiles(smiles)
+
+# Add Explicit hydrogen atoms
+molecule = Chem.AddHs(molecule)
+
+# Generate 3D Coordinates
+AllChem.EmbedMolecule(molecule)
+
+# Save the molecule in PDB Format
+output_file = "output_Hyd.pdb"
+with PDBWriter(output_file) as writer:
+    writer.write(molecule)
