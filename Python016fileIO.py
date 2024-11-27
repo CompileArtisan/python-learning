037e6bb98ffd76a56798166a18d0ecba2f4472bd
CompileArtisan <praanesh.b.nair@gmail.com>
2024-11-27 17:48:55 +0530

    Learnt about io.StringIO

--- Python016fileIO.py
+++ Python016fileIO.py
@@ -1,8 +1,26 @@
-# another way to open a file
-with open("Python015fileIO.txt", "a") as file:
+def fileIO():
+    # another way to open a file
+    with open("Python015fileIO.txt", "a") as file:
+        file.write("\n" + input("Enter something else: "))
+        
+    # this replaces this:
+    file = open("Python015fileIO.txt", "a")
     file.write("\n" + input("Enter something else: "))
-    
-# this replaces this:
-file = open("Python015fileIO.txt", "a")
-file.write("\n" + input("Enter something else: "))
-file.close()
+    file.close()
+
+
+
+def fileIO2():
+    import io
+    with io.StringIO() as file:
+        file.write("Hello")
+        file.seek(0)
+        print(file.read()) 
+        
+        
+    with io.StringIO("Hello world world") as file:
+        print(file.read())
+        
+if __name__ == "__main__":
+    # fileIO()
+    fileIO2()
